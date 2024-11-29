671788a4a8144d4ec414d08156eb5fb2c784c2c6
root <root@DESKTOP-MOVSMDF.>
2024-11-29 21:33:10 +0200

    basic gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,27 +1,62 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 char    *get_next_line(int fd)
 {
     int byte;
+    int i;
+    int x;
+    char c;
+    char *strbuff;
 
-    if (BUFFER_SIZE < 1 || fd < 0)
+    if (fd < 0)
         return (NULL);
     i = 0;
+    strbuff = (char *)malloc(10000000);
     if (!strbuff)
-        return (NULL);
-    str = read(fd, &c, 1); // reads every character in the file
+    {
+        free(strbuff);
+        return(NULL);
+    }
+    byte = read(fd, &c, 1); // reads every character in the file
     while (byte > 0)
     {
-        strbuff[i] = c;
-        i++;
         if (c == '\n' || c == EOF)
             break;
-        
-
+        else
+            strbuff[i] = c;
+            i++;
+            x--;
+        byte = read(fd, &c, 1);
     }
     while (i == 0 || byte < 0)
     {
-        free(BUFFER);
+        free(strbuff);
         return(NULL);
     }
+    strbuff[i] = '\0';
+    return (strbuff);
 
+}
+
+int main(void)
+{
+    int     fd;
+    int     i;
+    char    *str;
+    char    *path;
+
+    path = "tester.txt";
+    fd = open(path, O_RDONLY);
+    i = 0;
+    while (i < 20)
+    {
+        str = get_next_line(fd);
+        printf("[%i] %s\n",i, str);
+        i++;
+    }
+    close(fd);
+    return(0);
 }
