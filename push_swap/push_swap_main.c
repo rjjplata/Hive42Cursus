8109b986c37a7bd5c432f5a726fd882552ce36e4
rjjplata@gmail.com <rjjplata@gmail.com>
2025-01-10 14:14:31 +0200

    input checking

--- push_swap/push_swap_main.c
+++ push_swap/push_swap_main.c
@@ -4,8 +4,6 @@
 #include <limits.h>
 #include <stdlib.h>
 
-<<<<<<< HEAD:push_swap/push_swap_input.c
-
 static int checkarg1(char *str)
 {
     int i;
@@ -20,60 +18,14 @@ static int checkarg1(char *str)
             if(!(str[i + 1] >= '0' && str[i + 1] <= '9'))
                 return(0);
             i++;
-=======
-int ft_atoi(char *str)
-{
-    int result = 0;
-    int sign = 1;
-    int i = 0;
-
-    if (!str)
-        return (0);
-    while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-        i++;
-    if (str[i] == '-' || str[i] == '+')
-    {
-        if (str[i] == '-')
-            sign = -1;
-        i++;
-    }
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        result = result * 10 + (str[i] - '0');
-        i++;
-    }
-    return (sign * result);
-}
-
-int *ft_stratoi(const char *str)
-{
-    int *result;
-    int i;
-    int j;
-
-    i = 0;
-    j = 0;
-    while (str[i] != '\0')
-    {
-        while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-            i++;
-        if (str[i] != '\0')
-        {
-            result = ft_atoi(str[i]);
-            while ((str[i] >= '0' && str[i] <= '9') || str[i] == '-' || str[i] == '+')
-                i++;
->>>>>>> fdd97620334a02667574c4c61b359fb81af87d30:push_swap/push_swap_main.c
         }
         while(str[i] >= '0' && str[i] <= '9')
             i++;
         while(str[i] == ' ' || str[i] == '\t')
             i++;
     }
-<<<<<<< HEAD:push_swap/push_swap_input.c
     return(1);
 } 
-
-
 
 int main(int argc, char **argv)
 {
@@ -81,25 +33,15 @@ int main(int argc, char **argv)
     int *array;
     int i;
     int result;
+    long nbr;
+    int j;
 
+    array = NULL;
     if (argc == 1)
-=======
-    return(result);
-}
-
-int main(int argc, char **argv)
-{
-    int i;
-    char *x;
-
-    i =0;
-    if (argc < 2)
->>>>>>> fdd97620334a02667574c4c61b359fb81af87d30:push_swap/push_swap_main.c
     {
         printf("No arguments");
         return(0);
     }
-<<<<<<< HEAD:push_swap/push_swap_input.c
     if (argc == 2)
     {
             result = checkarg1(argv[1]);
@@ -113,31 +55,22 @@ int main(int argc, char **argv)
             }
             i++;
     }
-}
-=======
-    if (argc == 2 )
-    {
-        printf("Using ft_atoi\n");
-        i = 0;
-        x = (ft_split(argv[1], ' '));
-        x = ft_atoi(x);
-        while(x[i])
-        {
-            printf("Element [i]: %d", x[i]);
-            i++;
-        }
-    }
-    i = 0;
+    i = 1;
+    j = 0;
     if (argc > 2)
     {
-        printf("Using ft_stratoi\n");
-        ft_stratoi(argv[1]);
-        while(argv[1][i])
+   //     nbr = (int *)malloc(sizeof(int) * (argc - 1));
+        while (i < argc)
         {
-            printf("Element in the array[%d]: \n", argv[1][i]);
+            result = checkarg1(argv[i]);
+            if(result == 0)
+            {
+                printf("Error input");
+                return(0);
+            }
+ //           nbr[j] = (int)
+            printf("Input valid\n");
             i++;
         }
     }
-    return 0;
-} 
->>>>>>> fdd97620334a02667574c4c61b359fb81af87d30:push_swap/push_swap_main.c
+}
