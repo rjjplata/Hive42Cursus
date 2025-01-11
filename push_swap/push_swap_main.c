21a4f9dcfab51f2fd4b99aa365a5ce0c8e5b833c
rjjplata@gmail.com <rjjplata@gmail.com>
2025-01-11 23:29:52 +0200

    split done

--- push_swap/push_swap_main.c
+++ push_swap/push_swap_main.c
@@ -4,34 +4,6 @@
 #include <limits.h>
 #include <stdlib.h>
 
-char	**ft_split(char const *s, char c);
-
-/*
-static size_t countsubstr(char const *str, char c)
-{
-    int i;
-    unsigned int count;
-
-    i = 0;
-    count = 0;
-    if(str[i] == '\0')
-        return(0);
-    while(str[i] != '\0')
-    {
-        if(str[i] == c)
-            i++;
-        if(str[i] == '+' || str[i] == '-' || (str[i] >= '0' && str[i] <= '9'))
-        {
-            count++;
-            while(str[i] && (str[i] >= '0' && str[i] <= '9'))
-                i++;
-        }
-        i++;
-    }
-    return(count - 1);
-}
-*/
-
 static size_t countsubstr(char const *str)
 {
     int i = 0;
@@ -41,7 +13,7 @@ static size_t countsubstr(char const *str)
         return 0;
     while (str[i] != '\0')
     {
-        while (str[i] == ' ' && (str[i + 1] != '\0'))
+        while (str[i] == ' ' && (str[i] != '\0'))
             i++;
         if (str[i] == '+' || str[i] == '-' || (str[i] >= '0' && str[i] <= '9')) 
         {
@@ -52,6 +24,48 @@ static size_t countsubstr(char const *str)
                 i++;
     }
     return(count);
+}
+
+char *ft_strncpy(char *s1, char *s2, int n)
+{
+	int i = -1;
+
+	while (++i < n && s2[i])
+		s1[i] = s2[i];
+	s1[i] = '\0';
+	return (s1);
+}
+
+char    **ft_split(char *str, char c)
+{
+    char **result;
+    size_t substr;
+    size_t i;
+    size_t j;
+    size_t k;
+    size_t count;
+
+    count = countsubstr(str);
+    result = (char **)malloc(sizeof(char *) * (count + 1));
+    if(!str || !result)
+        return(NULL);
+    i = 0;
+    k = 0;
+    while(str[i] != '\0')
+    {
+        while(str[i] && str[i] == c)
+            i++;
+        j = i;
+        while(str[i] && ((str[i] == '+' || str[i] == '-') || (str[i] >= '0' && str[i] <= '9')))
+            i++;
+        if(i > j)
+        {
+            result[k] = (char *)malloc(sizeof(char) * ((i - j) + 1));
+            ft_strncpy(result[k++], &str[j], i - j);
+        }
+    }
+    result[k] = NULL;
+    return(result);
 }
 
 static int checkarg1(char *str)
@@ -98,9 +112,11 @@ int main(int argc, char **argv)
             result = checkarg1(argv[1]);
             if(result == 1)
             {
-                argnbr = countsubstr(argv[1]);
+                array = ft_split(argv[1], ' ');
+                printf("%s\n", array[1]);
+              //  argnbr = countsubstr(argv[1]);
                 printf("Number okay\n");
-                printf("Number of arguments: %d", argnbr);
+              //  printf("Number of arguments: %d", argnbr);
             }
             else if (result == 0)
             {
