dc7db2227df3f23aadf37830d5a8693cda3e02a2
Raniel Jerome Plata <rplata@c2r1p4.hive.fi>
2025-02-10 09:41:47 +0200

    added calloc and bzero

---
+++ push_swap/src/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rplata <rplata@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 16:36:55 by rplata            #+#    #+#             */
+/*   Updated: 2024/11/17 15:06:59 by rplata           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	ptr = (void *)malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero (ptr, nmemb * size);
+	return (ptr);
+}
